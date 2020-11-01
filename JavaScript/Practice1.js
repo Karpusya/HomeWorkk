2058ccbb28a6bcede883e4b39c75815b43fda55d
Oksana Karpevych <72864111+Karpusya@users.noreply.github.com>
2020-11-01 20:21:53 +0200

    01/11/2020

---
+++ JavaScript/Practice1.js
@@ -0,0 +1,274 @@
+// присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false
+
+//let hello = 'hello';
+//let owu = 'owu';
+//let com = 'com';
+//let ua = 'ua';
+//let  one = '1';
+//let ten = '10';
+//let minus999= '-999';
+//let b123 = '123';
+//let c3point14 = '3.14';
+//let d2point7 = '2.7';
+//let sixteen = '16';
+//let truetrue = 'true';
+//let falsefalse = 'false';
+
+// Вывести каждую при помощи console.log , alert, document.write
+
+//console.log(hello, owu, com, ua, one, ten, minus999, b123, c3point14, d2point7, sixteen, truetrue, falsefalse);
+
+// alert(hello);
+// alert(owu);
+// alert(com);
+// alert(ua);
+// alert(one);
+// alert(ten);
+// alert(minus999);
+// alert(b123);
+// alert(c3point14);
+// alert(d2point7);
+// alert(sixteen);
+// alert(truetrue);
+// alert(falsefalse);
+
+// document.write(hello);
+// document.write(owu);
+// document.write(com);
+// document.write(ua);
+// document.write(one);
+// document.write(ten);
+// document.write(minus999);
+// document.write(b123);
+// document.write(c3point14);
+// document.write(d2point7);
+// document.write(sixteen);
+// document.write(truetrue);
+// document.write(falsefalse);
+
+// переопределить каждую переменную из задания 1 дав им произвольные значения;
+
+// hello = 'hi';
+// owu = 'courses';
+// com = 'internet';
+// ua = 'ukraine';
+// one = 'first';
+// ten = 'tenth';
+// minus999 ='negative';
+// b123 = 'beginning';
+// c3point14 = 'pi';
+// d2point7 = 'twoseven';
+// sixteen = 'teenager';
+// truetrue = 'right';
+// falsefalse = 'wrong';
+
+//Вывести каждую при помощи console.log , alert, document.write
+
+
+//console.log(hello, owu, com, ua, one, ten, minus999, b123, c3point14, d2point7, sixteen, truetrue, falsefalse);
+
+//alert(hello);
+//alert(owu);
+// alert(com);
+// alert(ua);
+// alert(one);
+// alert(ten);
+// alert(minus999);
+// alert(b123);
+// alert(c3point14);
+// alert(d2point7);
+// alert(sixteen);
+// alert(truetrue);
+// alert(falsefalse);
+
+// document.write(hello);
+// document.write(owu);
+// document.write(com);
+// document.write(ua);
+// document.write(one);
+// document.write(ten);
+// document.write(minus999);
+// document.write(b123);
+// document.write(c3point14);
+// document.write(d2point7);
+// document.write(sixteen);
+// document.write(truetrue);
+// document.write(falsefalse);
+
+//Создать 3 числовых и 3 стринговых константы.
+
+// const myBirthday = 05.03.1990;
+// const mymotherbday = 16.04.1961;
+// const myfatherbday = 20.03.1956;
+
+// const me = '05.03.1990';
+// const mom = '16.04.1961';
+// const dad = '20.03.1956';
+
+//Вывести каждую при помощи console.log , alert, document.write
+
+//console.log(myBirthday, mymotherbday myfatherbday, me, mom, dad)
+
+// alert(myBirthday);
+// alert(mymotherbday);
+// alert(myfatherbday);
+// alert(me);
+// alert(mom);
+// alert(dad);
+
+// document.write (myBirthday);
+// document.write (myfatherbday);
+// document.write (myfatherbday);
+// document.write (me);
+// document.write (mom);
+// document.write (dad);
+
+// при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО. Для фамилии имени и отчества создать разные переменные;
+
+// let firstname1 = prompt('Введіть ваше ім\'я:');
+// let surname1 = prompt('Введіть ваше прізвище:');
+// let middlename1 = prompt('Введіть ваше по-батькові:');
+
+
+// //Вывести каждую при помощи console.log , alert, document.write
+
+
+// console.log(firstname1, surname1, middlename1);
+
+// alert(firstname1);
+// alert(surname1);
+// alert(middlename1);
+
+
+// document.write(firstname1);
+// document.write(surname1);
+// document.write(middlename1);
+
+// //Взять переменные из задания 4 и сконкатенировать их в одной переменной person.
+
+// let person = firstname1 + ' ' + surname1 + ' ' + middlename1;
+// console.log(person);
+
+//Взять задние 4 и 5 и применить его к ФИО всех членов своей семьи.
+
+// let firstname2 = prompt('Введіть ім\'я Вашої матері:');
+// let surname2 = prompt('Введіть прізвище Вашої матері:');
+// let middlename2 = prompt('Введіть по-батькові Вашої матері:');
+
+// console.log(firstname2, surname2, middlename2);
+
+// alert(firstname2);
+// alert(surname2);
+// alert(middlename2);
+
+
+// document.write(firstname2);
+// document.write(surname2);
+// document.write(middlename2);
+
+
+// let firstname3 = prompt('Введіть ім\'я Вашого батька:');
+// let surname3 = prompt('Введіть прізвище Вашого батька:');
+// let middlename3 = prompt('Введіть по-батькові Вашого батька:');
+
+// console.log(firstname3, surname3, middlename3);
+
+// alert(firstname3);
+// alert(surname3);
+// alert(middlename3);
+
+
+// document.write(firstname3);
+// document.write(surname3);
+// document.write(middlename3);
+
+//при помощи prompt() получить 3 числа. Привести их к числовому типу при помощи +. вывести их в консоль.
+
+// let number1 = +prompt('Введіть число 1');
+// let number2 = +prompt('Введіть число 2');
+// let number3 = +prompt('Введіть число 3');
+
+// console.log(number1, number2, number3);
+
+
+//при помощи prompt() получить 4 числа. Привести их к числовому типу при помощи parseInt. Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+
+// let number4 = parseInt(prompt('Введіть число 4'));
+// let number5 = parseInt(prompt('Введіть число 5'));
+// let number6 = parseInt(prompt('Введіть число 6'));
+// let number7 = parseInt(prompt('Введіть число 7'));
+
+// let result1 =  number4 +  number5 + number6 + number7 ;
+
+// console.log (result);
+
+//при помощи prompt()  получить 3 числа с плавающей точекой. при помощи parseFloat привести их к соответсвующему типу.
+
+// let number8 = parseFloat(prompt('Введіть число 8'));
+// let number9 = parseFloat(prompt('Введіть число 9'));
+// let number10 = parseFloat(prompt('Введіть число 10'));
+
+//Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+
+// let result2 =  number8 +  number9 + number10;
+
+// console.log (result2);
+
+//при помощи prompt()  получить 3 числа с плавающей точекой. Округлить их при помощи Math.round Сложить их между собой записав результат в переменную  и вывести в консоль браузера
+
+// let number11 = Math.round(parseFloat(prompt('Введіть число 11')));
+// let number12 = Math.round(parseFloat(prompt('Введіть число 12')));
+// let number13 = Math.round(parseFloat(prompt('Введіть число 13')));
+
+// let result3 =  number11 +  number12 + number13;
+
+// console.log (result3);
+
+//при помощи prompt()  получить 2 целых числа. Привести их к целочисленному типу. Первое число - это число которое будут возводить в степень. Второе число - это число которое является степенью. При помощи Math.pow возвести первое число в степень второго числа.
+
+// let number14 = +prompt('Введіть число');
+// let number15 = +prompt('Введіть степінь');
+
+// console.log(Math.pow(number14, number15));
+
+//При помощи оператора опредеоения типа typeof определить типы следующих переменных и вывести их в консоль let a = 100; let b = '100'; let c = true; let d = undefined;
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+// Поставьте соответствующий оператор в выражениях что бы получился соответсвующий результат.
+// В одинаковых выражениях в задании не использовать два одинаковых оператора.
+// Каждое выражение вложить в свою переменную
+// 5 ? 6 -> true
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 10 ? 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 ? '123' -> false
+// 123 ? '123' -> true
+
+
+// let number14 = 5<6;
+// let number15 = 5>6;
+// let number16 = 5==6;
+// let number17 = 5>=6;
+// let number18 = 10==10;
+// let number19 = 10>=10;
+// let number20 = 10>10;
+// let number21 = 10<10;
+// let number22 =  10!=10;
+// let number23 = 123==='123';
+// let number24 = 123=='123';
+
+// console.log (number14, number15, number16, number17, number18, number19, number20, number21, number22, number23, number24);
